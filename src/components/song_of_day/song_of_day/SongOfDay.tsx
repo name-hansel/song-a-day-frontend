3bd991aa998cc6dde5bfe4870f299e8af864aa5d
Snehal Mane <snehal6mane@gmail.com>
2026-03-01 21:18:04 +0530

    Remove unnecessary component

--- src/components/song_of_day/song_of_day/SongOfDay.tsx
+++ src/components/song_of_day/song_of_day/SongOfDay.tsx
@@ -7,7 +7,6 @@ import {
     type SongOfDay as SongOfDayType,
     type SongOfDay
 } from "../../../api/song.ts";
-import SongOfDayItem from "../song_of_day_item/SongOfDayItem.tsx";
 import Spinner from "../../../pages/spinner/Spinner.tsx";
 
 export default function SongOfDay({song, setSong}: {
@@ -54,7 +53,34 @@ export default function SongOfDay({song, setSong}: {
     }
 
     return (
-        <SongOfDayItem song={song}
-                       removeSong={removeSongForAppUser}/>
+        <div className="song-card">
+            <div className="song-image-wrapper">
+                <img
+                    src={song.trackInformation.imageUrl}
+                    alt="Song cover"
+                    className="song-image"
+                />
+            </div>
+            <div className="song-details">
+                <div className="track-details">
+                    <a target="_blank"
+                       className="song-title track-link"
+                       href={song.trackInformation.spotifyUrl}><h3
+                        className="song-title">{song.trackInformation.trackName}</h3>
+                    </a>
+                    <p className="song-artist">{song.trackInformation.artistName}</p>
+                    <p className="song-album">{song.trackInformation.albumName}</p>
+                </div>
+                <div className="song-metainfo">
+                    <p className="logged-at">
+                        logged at: {song.addedAtTime}
+                    </p>
+                    <button className="remove-button"
+                            onClick={removeSongForAppUser}>
+                        remove song of day
+                    </button>
+                </div>
+            </div>
+        </div>
     )
 }
