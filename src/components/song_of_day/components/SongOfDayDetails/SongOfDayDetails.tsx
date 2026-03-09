b448a026ba83c8200667d21a8b053baa3d31801a
Snehal Mane <snehal6mane@gmail.com>
2026-03-09 23:53:58 +0530

    Link to spotify track on song title

--- src/components/song_of_day/components/SongOfDayDetails/SongOfDayDetails.tsx
+++ src/components/song_of_day/components/SongOfDayDetails/SongOfDayDetails.tsx
@@ -6,10 +6,12 @@ export default function SongOfDayDetails({trackInformation}: {
 }) {
     return (
         <div className="song-of-day-entry-track-info">
-            <h2 className="song-of-day-track-name"
-                title={trackInformation.trackName}>
-                {trackInformation.trackName}
-            </h2>
+            <a href={trackInformation.spotifyUrl} target="_blank">
+                <h2 className="song-of-day-track-name"
+                    title={trackInformation.trackName}>
+                    {trackInformation.trackName}
+                </h2>
+            </a>
             <p className="song-of-day-artist-name">
                 {trackInformation.artistName}
             </p>
