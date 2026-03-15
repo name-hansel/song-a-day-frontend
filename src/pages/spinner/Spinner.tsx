c8cf8d3ede29632ecf81285fdcfea2a7dfa86f8c
Snehal Mane <snehal6mane@gmail.com>
2026-03-15 23:23:00 +0530

    Optional param

--- src/pages/spinner/Spinner.tsx
+++ src/pages/spinner/Spinner.tsx
@@ -2,7 +2,7 @@ import {Loader2} from "lucide-react";
 import "./Spinner.css";
 
 export default function Spinner({size = "2rem"}: {
-    size: string
+    size?: string
 }) {
     return <div className="spinner-overlay" style={{fontSize: size}}>
         <Loader2 className="spinner-icon"/>
