3a2559e5ef61f20f3874dd9970cbf3701040f492
Snehal Mane <snehal6mane@gmail.com>
2026-03-17 00:22:58 +0530

    Collapsible sidebar

--- src/components/home_sidebar/HomeSidebar.tsx
+++ src/components/home_sidebar/HomeSidebar.tsx
@@ -6,6 +6,7 @@ import {getErrorMessage} from "../../api/messages.ts";
 import ErrorBanner from "../error_banner/ErrorBanner.tsx";
 import type {SongOfDay} from "../../types/SongOfDay.ts";
 import {getUserSongHistoryForWeek} from "../../api/song.ts";
+import {ArrowLeft, ArrowRight} from "lucide-react";
 
 export default function HomeSidebar({songForToday}: {
     songForToday?: SongOfDay
@@ -13,6 +14,7 @@ export default function HomeSidebar({songForToday}: {
     const [songHistory, setSongHistory] = useState<SongOfDay[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState<string | null>(null);
+    const [collapsed, setCollapsed] = useState(false);
 
     useEffect(() => {
         async function getSongHistory() {
@@ -33,25 +35,49 @@ export default function HomeSidebar({songForToday}: {
     }, [songForToday])
 
     return (
-        <aside className="home-sidebar">
-            <div className="sidebar-title">
-                week so far
-            </div>
+        <aside className={`home-sidebar ${collapsed ? "collapsed" : ""}`}>
             {
-                loading && <Spinner/>
-            }
-            {
-                error && <ErrorBanner message={error}/>
-            }
-            {
-                !error &&
-                <div className="sidebar-content">
-                    {songHistory && songHistory.map((song: SongOfDay, index: number) => (
-                        <HomeSidebarSong song={song}
-                                         key={index}
-                                         isLatest={index === songHistory.length - 1}/>
-                    ))}
-                </div>
+                collapsed ? <button
+                        className="sidebar-expand-btn"
+                        onClick={() => setCollapsed(false)}
+                        aria-label="Expand sidebar"
+                    >
+                        <ArrowRight size={16}/>
+                    </button> :
+                    <>
+                        <div className="sidebar-header">
+                            <div className="sidebar-title">week so far</div>
+
+                            <button
+                                className="sidebar-collapse-btn"
+                                onClick={() => setCollapsed(true)}
+                                aria-label="Collapse sidebar"
+                            >
+                                <ArrowLeft size={16}/>
+                            </button>
+                        </div>
+                        {
+                            loading && <Spinner/>
+                        }
+                        {
+                            error && <ErrorBanner message={error}/>
+                        }
+                        {
+                            !error &&
+                            <div className="sidebar-content">
+                                {
+                                    songHistory &&
+                                    songHistory.map((song: SongOfDay, index: number) => (
+                                        <HomeSidebarSong
+                                            song={song}
+                                            key={index}
+                                            isLatest={index === songHistory.length - 1}
+                                        />
+                                    ))
+                                }
+                            </div>
+                        }
+                    </>
             }
         </aside>
     )
