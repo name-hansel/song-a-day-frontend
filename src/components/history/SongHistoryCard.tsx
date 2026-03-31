92ea0bfb5017d785fae3bc2dc06ec50a685dde6d
Snehal Mane <snehal6mane@gmail.com>
2026-03-31 23:56:41 +0530

    Link history card to song entry

--- src/components/history/SongHistoryCard.tsx
+++ src/components/history/SongHistoryCard.tsx
@@ -1,29 +1,32 @@
 import type {SongOfDay} from "../../types/SongOfDay.ts";
 import "./SongHistoryCard.css"
 import {getDateNumericFromDate} from "../../utils/DateUtils.ts";
+import {Link} from "react-router";
 
 export default function SongHistoryCard({song}: {
     song: SongOfDay
 }) {
     return (
-        <div className="entry-row">
-            <div className="entry-date">{getDateNumericFromDate(song.songDate)}</div>
-            <div className="entry-card">
-                <img
-                    src={song.trackInformation.smallImageUrl}
-                    alt="album"
-                    className="entry-album"
-                />
+        <Link to={`/song-a-day/${song.songDate}`}>
+            <div className="entry-row">
+                <div className="entry-date">{getDateNumericFromDate(song.songDate)}</div>
+                <div className="entry-card">
+                    <img
+                        src={song.trackInformation.smallImageUrl}
+                        alt="album"
+                        className="entry-album"
+                    />
 
-                <div className="entry-info">
-                    <div className="entry-track">{song.trackInformation.trackName}</div>
-                    <div className="entry-artist">{song.trackInformation.artistName}</div>
-                </div>
+                    <div className="entry-info">
+                        <div className="entry-track">{song.trackInformation.trackName}</div>
+                        <div className="entry-artist">{song.trackInformation.artistName}</div>
+                    </div>
 
-                <div className="entry-action">
-                    <span className="entry-icon">›</span>
+                    <div className="entry-action">
+                        <span className="entry-icon">›</span>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     )
 }
