b713a140ed3e2df6d23684969fa6bd42c1ebc46b
Snehal Mane <snehal6mane@gmail.com>
2026-04-01 22:50:42 +0530

    Rename classNames and remove link text decoration from date

--- src/components/history/SongHistoryCard.tsx
+++ src/components/history/SongHistoryCard.tsx
@@ -7,26 +7,27 @@ export default function SongHistoryCard({song}: {
     song: SongOfDay
 }) {
     return (
-        <Link to={`/song-a-day/${song.songDate}`}>
-            <div className="entry-row">
-                <div className="entry-date">{getDateNumericFromDate(song.songDate)}</div>
-                <div className="entry-card">
+
+        <div className="song-history-row">
+            <div className="song-history-date">{getDateNumericFromDate(song.songDate)}</div>
+            <Link to={`/song-a-day/${song.songDate}`} className="song-history-link">
+                <div className="song-history-card">
                     <img
                         src={song.trackInformation.smallImageUrl}
                         alt="album"
-                        className="entry-album"
+                        className="song-history-album-img"
                     />
 
-                    <div className="entry-info">
-                        <div className="entry-track">{song.trackInformation.trackName}</div>
-                        <div className="entry-artist">{song.trackInformation.artistName}</div>
+                    <div className="song-history-track-info">
+                        <div className="song-history-track-name">{song.trackInformation.trackName}</div>
+                        <div className="song-history-track-artist">{song.trackInformation.artistName}</div>
                     </div>
 
-                    <div className="entry-action">
-                        <span className="entry-icon">›</span>
+                    <div className="song-history-action">
+                        <span className="song-history-action-icon">›</span>
                     </div>
                 </div>
-            </div>
-        </Link>
+            </Link>
+        </div>
     )
 }
