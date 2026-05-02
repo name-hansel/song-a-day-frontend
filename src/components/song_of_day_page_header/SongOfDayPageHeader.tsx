8223ad5988b8b4f621ed83fed8fcab4704ef8025
Snehal Mane <snehal6mane@gmail.com>
2026-05-02 19:55:35 +0530

    Home button functionality

--- src/components/song_of_day_page_header/SongOfDayPageHeader.tsx
+++ src/components/song_of_day_page_header/SongOfDayPageHeader.tsx
@@ -1,14 +1,21 @@
 import "./SongOfDayPageHeader.css";
 import SearchBar from "../search_bar/SearchBar.tsx";
 import {ArrowLeft, ArrowRight, Home} from "lucide-react";
+import {useNavigate} from "react-router";
 
 
 export default function SongOfDayPageHeader({onSelect}: {
     onSelect?: (trackId: string) => void,
 }) {
+    const navigate = useNavigate();
+
+    function goHome() {
+        navigate("/");
+    }
+
     return (
         <section className="song-a-day-page-header-section">
-            <button className="song-a-day-page-header-btn">
+            <button onClick={goHome} className="song-a-day-page-header-btn">
                 <Home size={18}/>
             </button>
             {
