643a72cd3a7062d90366db4c7ec6cf11514c3514
Snehal Mane <snehal6mane@gmail.com>
2026-05-03 17:30:56 +0530

    Do not allow to type future date

--- src/components/log_song_confirmation/footer/LogSongConfirmationFooter.tsx
+++ src/components/log_song_confirmation/footer/LogSongConfirmationFooter.tsx
@@ -2,6 +2,7 @@ import Button from "../../common/button/Button.tsx";
 import {useRequiredAuth} from "../../../context/AuthContext.tsx";
 import {getTodayForTimezone} from "../../../utils/DateUtils.ts";
 import "./LogSongConfirmationFooter.css"
+import type {ChangeEvent} from "react";
 
 export default function LogSongConfirmationFooter({onConfirmation, confirmLoading, date, setDate, onCancel}: {
     onConfirmation: () => Promise<void>,
@@ -10,7 +11,19 @@ export default function LogSongConfirmationFooter({onConfirmation, confirmLoadin
     setDate: (value: (((prevState: string) => string) | string)) => void,
     onCancel: () => void
 }) {
-    const appUser = useRequiredAuth();
+    const {timezone} = useRequiredAuth();
+
+    function handleDateChange(e: ChangeEvent<HTMLInputElement, HTMLInputElement>) {
+        const value = e.target.value;
+        const today = getTodayForTimezone(timezone);
+
+        if (value > today) {
+            e.target.value = today;
+            return;
+        }
+
+        setDate(value);
+    }
 
     return <div className="song-of-day-entry-footer">
         <div
@@ -20,8 +33,8 @@ export default function LogSongConfirmationFooter({onConfirmation, confirmLoadin
             <input
                 type="date"
                 value={date}
-                onChange={(e) => setDate(e.target.value)}
-                max={getTodayForTimezone(appUser.timezone)}
+                onChange={(e) => handleDateChange(e)}
+                max={getTodayForTimezone(timezone)}
                 className="log-song-confirmation-date-picker"/>
         </div>
         <Button onClick={onCancel}
